8308fd93022ce9227e2c947ae092c10b291e4726
RheshavMaharaj <rheshav@hotmail.com>
2020-07-20 18:22:42 +1000

    Minor changes to home page for testing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="Toolbar">
+        <h1>
+          Home Icon and blah blah blah
+        </h1>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -12,13 +17,18 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Saurab sucks big dick everyday
         </a>
+        <p>
+          Insert App functionality here. Welcome to the matrix
+          This is HTML on the page. it is inside a container.
+        </p>
       </header>
+      
     </div>
   );
 }
