f640865e27f6a47fe29975eb91f3250111decd6e
RheshavMaharaj <rheshav@hotmail.com>
2020-07-22 21:32:35 +1000

    Navbar Functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import React from 'react';
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import about from './Assets/help.svg';
+import home from './Assets/home-run.svg';
+import user from './Assets/user.svg';
+
+import Home from './Home.js';
+import About from './About.js';
+import User from './User.js';
+
+export default function App() {
   return (
-    <div className="App">
-      <div className="Toolbar">
-        <h1>
-          Home Icon and blah blah blah
-        </h1>
+    <Router>
+      <div>
+        <div className="Toolbar">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Pheonix Industries</h1>
+          <nav>
+            <Link to="/"><img src={home} className="App-logo" alt="home" /></Link>
+            <Link to="/about"><img src={about} className="App-logo" alt="about" /></Link>
+            <Link to="/users"><img src={user} className="App-logo" alt="user" /></Link>
+          </nav>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <User />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Saurab sucks big dick everyday
-        </a>
-        <p>
-          Insert App functionality here. Welcome to the matrix
-          This is HTML on the page. it is inside a container.
-        </p>
-      </header>
-      
-    </div>
+    </Router>
   );
 }
-
-export default App;
